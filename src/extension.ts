f45c573c944ba6f7980af8a54d5239c439456a2a
Sylvain Reynaud <sylvainreynaud@protonmail.com>
2021-08-14 18:42:50 +0200

    wip: show value of env var next to ${VAR}

--- src/extension.ts
+++ src/extension.ts
@@ -13,37 +13,99 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let timeout: NodeJS.Timer | undefined = undefined;
 
-	const envVarDecoratorType = vscode.window.createTextEditorDecorationType({
-		after: {
-			contentText: "a bien coucou",
-			color: "green",
-		},
-		isWholeLine: true,
-	});
-
 	let activeEditor = vscode.window.activeTextEditor;
 
-	function readDotEnvFile() {
-		
+	function parseDotEnvText (text: string): {} {
+		const NEWLINE = '\n'
+		const RE_INI_KEY_VAL = /^\s*([\w.-]+)\s*=\s*(.*)?\s*$/
+		const RE_NEWLINES = /\\n/g
+		const NEWLINES_MATCH = /\r\n|\n|\r/
+
+		// TODO: demander au prof de ts
+		const envVars: any = {};
+
+		// convert Buffers before splitting into lines and processing
+		const lines = text.toString().split(NEWLINES_MATCH);
+		lines.forEach(function (line: string, idx: number) {
+			// matching "KEY' and 'VAL' in 'KEY=VAL'
+			const keyValueArr = line.match(RE_INI_KEY_VAL)
+			// matched?
+			if (keyValueArr != null) {
+				const key = keyValueArr[1]
+				// default undefined or missing values to empty string
+				let val = (keyValueArr[2] || '')
+				const end = val.length - 1
+				const isDoubleQuoted = val[0] === '"' && val[end] === '"'
+				const isSingleQuoted = val[0] === "'" && val[end] === "'"
+
+				// if single or double quoted, remove quotes
+				if (isSingleQuoted || isDoubleQuoted) {
+					val = val.substring(1, end)
+
+					// if double quoted, expand newlines
+					if (isDoubleQuoted) {
+						val = val.replace(RE_NEWLINES, NEWLINE)
+					}
+				} else {
+					// remove surrounding whitespace
+					val = val.trim()
+				}
+
+				envVars[key] = val;
+			} else {
+				console.log(`did not match key and value when parsing line ${idx + 1}: ${line}`)
+			}
+		})
+		return envVars
 	}
 
-	function updateDecorations() {
+	async function getEnvVarsFromDotEnvFile() {
+		// vscode.workspace.findFiles('**/.env', '**/.env.local', '**/.env.local.dist', '**/.env.dist').then(files => {
+		const files = await vscode.workspace.findFiles('**/.env')
+		if (files.length > 0) {
+			const doc = await vscode.workspace.openTextDocument(files[0])
+			const text = doc.getText()
+			const envVars = parseDotEnvText(text);
+			return envVars;
+		}
+		return {};
+	}
+
+	async function updateDecorations() {
 		if (!activeEditor) {
 			return;
 		}
-		readDotEnvFile();
+		const envVars = await getEnvVarsFromDotEnvFile();
 		const regEx = /\$\{([A-Za-z0-9_.-]+)(?::([^\\}]*))?\}/g;
 		const text = activeEditor.document.getText();
-		const envVars: vscode.DecorationOptions[] = [];
 		let match;
 		while ((match = regEx.exec(text))) {
 			console.log('match:', match)
+
+			// find the env var in the envVars array
+			// TODO: demander au prof de ts
+			console.log('envVars:', envVars)
+			console.log('match[0]:', match[0])
+			//@ts-ignore
+			console.log('envVars[match[0]]:', envVars[match[1]])
+			//@ts-ignore
+			let matchedEnvVar: string = envVars[match[1]] || 'undefined';
+			console.log('matchedEnvVar:', matchedEnvVar)
+
 			const startPos = activeEditor.document.positionAt(match.index);
 			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
 			const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Environment variable **' + match[0] + '**' };
-			envVars.push(decoration);
+
+			const envVarDecoratorType = vscode.window.createTextEditorDecorationType({
+				after: {
+					contentText: "\t" + matchedEnvVar,
+					color: "green",
+				},
+				// isWholeLine: true,
+			});
+			activeEditor.setDecorations(envVarDecoratorType, [decoration]);
 		}
-		activeEditor.setDecorations(envVarDecoratorType, envVars);
+		
 	}
 
 	function triggerUpdateDecorations() {
